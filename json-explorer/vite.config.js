664e1824c48dc33e9d4c0403478314645071b49b
Christian Bernecker <Christian.Bernecker@example.com>
2025-04-07 10:11:42 +0200

    Add environment variable in Vite config

--- json-explorer/vite.config.js
+++ json-explorer/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: '/',
-})
+  define: {
+    'import.meta.env.VITE_ENVIRONMENT': JSON.stringify(mode)
+  }
+}))
